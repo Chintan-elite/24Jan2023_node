1e70544aac68ff56e2e9c8b1f5817054cb1f435b
Chintan <chintan.tops@gmail.com>
2023-01-24 09:16:13 +0530

    js

---
+++ 001_JSLearning/app.js
@@ -0,0 +1,44 @@
+//console.log("testing....")
+
+//variables 
+
+// var a = 10;
+// var b = 10.23;
+// var c = 'a';
+// var d = "tops";
+// var e = [10, 20, 30, 40, "tops", 88.66]
+// var f;
+// var g = null;
+// var h = true;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+//datatypes: 
+//number,string,boolean
+//object,array,undefine
+
+// console.log(typeof (a));
+// console.log(typeof (c));
+// console.log(typeof (e));
+// console.log(typeof (f));
+// console.log(typeof (g));
+// console.log(typeof (h));
+//***************oprators*******************
+//arithmatic 
+// var a = 10;
+// var b = 20;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b);
+
+//*************************** */
+//conditional if-else, switch-case
+
+//looping for, while, do-while
+
+
