e66e6a26e08410992bc88367bf7857e3cebb67f0
Chintan-elite <thecsquare.elite@gmail.com>
2023-01-26 09:29:22 +0530

    js learn

--- 001_JSLearning/app.js
+++ 001_JSLearning/app.js
@@ -41,4 +41,103 @@
 
 //looping for, while, do-while
 
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
 
+// for (var i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+//var i = 10;
+
+// while (i > 10) {
+//     console.log(i);
+//     i--;
+// }
+
+// do {
+//     console.log(i);
+// } while (i > 10);
+
+
+//function
+
+function call() {
+    console.log("calling...");
+}
+
+function add(a, b = 5) {
+    console.log(a + b);
+}
+
+function square(a) {
+    return a * a;
+}
+
+// call();
+// add(10, 20)
+// var sq = square(10)
+// console.log(sq);
+// console.log(square(50));
+
+//add(10)
+
+
+//arrow function
+// var call = () => {
+//     console.log("calling");
+// }
+
+// var add = (a, b) => {
+//     console.log(a + b);
+// }
+
+// call()
+// add(10, 20)
+
+
+//array : 
+var a = [10, 20, 30, 40, 50, 60, 30, "abc"];
+
+// console.log(a.length);
+// console.log(a.pop());
+// console.log(a);
+// console.log(a.pop());
+// console.log(a);
+// console.log(a.shift());
+// a.unshift(100)
+// console.log(a);
+// a.push(200)
+// console.log(a);
+
+// for (var i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+//return single value
+// var myval1 = a.find(ele => {
+//     return ele == "abc";
+// })
+
+//return array
+// var myval2 = a.filter(ele => {
+//     return ele == "abc";
+// })
+
+// console.log(myval1);
+// console.log(myval2);
+
+
+
+var str = " sun rises in east ";
+
+console.log(str.length);
+console.log(str.charAt(5));
+console.log(str.indexOf("s"));
+console.log(str.lastIndexOf("s"));
+console.log(str.endsWith("a"));
+console.log(str.split(" "));
+console.log(str.trim());
+console.log(str.toUpperCase());
+console.log(str.substring(5, 12));
